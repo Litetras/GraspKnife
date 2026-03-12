fdfa8766c5337161ef12f431fc1915b6b7447c8c
Litetras <3060219886@qq.com>
2026-03-12 23:06:57 +0800

    多物体训练小bug

--- grasp_gen/dataset/dataset.py
+++ grasp_gen/dataset/dataset.py
@@ -460,7 +460,7 @@ class PickDataset(Dataset):
             logger.info("No filtering; All datapoints are used")
 
         self.overfitting_mode = False
-        if len(self.scenes) == 1:
+        if len(self.scenes) <= 10:  # If the dataset is very small, we are likely in overfitting/debugging mode. In that case, we can repeat the scenes multiple times to allow for more iterations per epoch, which can help with convergence and debugging.
             multiplier = 100 if self.split == "train" else 10
             if self.split != "train" and not rotation_augmentation:
                 multiplier = 1
